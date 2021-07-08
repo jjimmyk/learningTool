38dd98f5482acb6bca224b1ca8c44ddf21c2de39
TeacherKing47 <32286815+jjimmyk@users.noreply.github.com>
2021-07-08 14:52:50 -0700

    Update firebase-connector.js

--- scripts/firebase-connector.js
+++ scripts/firebase-connector.js
@@ -1,21 +1,4 @@
 
-
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  var firebaseConfig = {
-    apiKey: "AIzaSyC72q6mL6d52YuTOPsRx5KDSRdrfiNotVk",
-    authDomain: "learningtool-54b95.firebaseapp.com",
-    projectId: "learningtool-54b95",
-    storageBucket: "learningtool-54b95.appspot.com",
-    messagingSenderId: "844271141498",
-    appId: "1:844271141498:web:e3f8dee7debd59c6d8f1c3",
-    measurementId: "G-H92HYQ7EVH"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  firebase.analytics();
-
-
 let dbLocation = firebase.database().ref('LearningTool1');
 
 dbLocation.set(myFlashcards);
